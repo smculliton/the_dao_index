526d7876d500505e599407c3c3bd5e06a257ada7
Sean Culliton <smculliton@gmail.com>
2023-01-09 13:25:45 -0700

    Add stylizing

--- app/models/hot_take.rb
+++ app/models/hot_take.rb
@@ -2,10 +2,10 @@ class HotTake < ApplicationRecord
   validates_presence_of :description
 
   def self.hottest_takes
-    HotTake.select('*, hot_votes - cold_votes as index').order(index: :desc).limit(5)
+    HotTake.select('*, hot_votes - cold_votes as index').where('hot_votes - cold_votes > 0').order(index: :desc).limit(5)
   end
 
   def self.coldest_takes
-    HotTake.select('*, hot_votes - cold_votes as index').order(index: :asc).limit(5)
+    HotTake.select('*, hot_votes - cold_votes as index').where('hot_votes - cold_votes < 0').order(index: :asc).limit(5)
   end
 end
