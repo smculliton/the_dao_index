8ed2e6c564d7916cba0498b44a0fdb3267105c3f
Sean Culliton <smculliton@gmail.com>
2023-01-13 11:03:01 -0700

    Add vote buttons to heat rankings, change limit of takes to 10

--- app/models/hot_take.rb
+++ app/models/hot_take.rb
@@ -2,10 +2,10 @@ class HotTake < ApplicationRecord
   validates_presence_of :description
 
   def self.hottest_takes
-    HotTake.select('*, hot_votes - cold_votes as index').where('hot_votes - cold_votes > 0').order(index: :desc).limit(5)
+    HotTake.select('*, hot_votes - cold_votes as index').where('hot_votes - cold_votes > 0').order(index: :desc).limit(10)
   end
 
   def self.coldest_takes
-    HotTake.select('*, hot_votes - cold_votes as index').where('hot_votes - cold_votes < 0').order(index: :asc).limit(5)
+    HotTake.select('*, hot_votes - cold_votes as index').where('hot_votes - cold_votes <= 0').order(index: :asc).limit(10)
   end
 end
